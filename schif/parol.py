872a7b3d0658d149bf6b70b3365da8dabde63d73
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2022-12-24 16:02:50 -0500

    added helper methods

--- schif/parol.py
+++ schif/parol.py
@@ -10,6 +10,24 @@ class Parol:
         if not self.password_file.parent.exists():
             self.password_file.parent.mkdir()
         elif not self.key_file.exists():
-            ... # TODO: make a key generation method
+            self.create_key()
         else:
             ... # TODO: make a load passwords method
+
+    def create_key(self) -> None:
+        """Create an encryption key file."""
+        with open(self.key_file, "wb") as f:
+            f.write(Fernet.generate_key())
+
+    def load_key(self) -> bytes:
+        """Load an existing key file."""
+        with open(self.key_file, 'rb') as f:
+            return f.readline()
+
+    def update_key_file(self, file: str) -> None:
+        """Update the key file."""
+        self.key_file = pathlib.Path(file).absolute()
+
+    def update_password_file(self, file: str) -> None:
+        """Update the password file."""
+        self.password_file = pathlib.Path(file).absolute()
