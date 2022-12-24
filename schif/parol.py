20a1ddf9ef295c66154807c3f9abec3064436597
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2022-12-24 15:57:44 -0500

    defined the parol initialization

--- schif/parol.py
+++ schif/parol.py
@@ -1,8 +1,15 @@
 from cryptography.fernet import Fernet
 
+import pathlib
+
 class Parol:
 
-    def __init__(self, data_directory: str = "./data", key_file: str = "./KEY", password_file: str = "./passwords.db") -> None:
-        self.data_directory = data_directory
-        self.key_file = key_file
-        self.password_file = password_file
+    def __init__(self, data_directory: str = "./data", key_filename: str = "KEY", password_filename: str = "passwords.db") -> None:
+        self.key_file = pathlib.Path(f"{data_directory}/{key_filename}").absolute()
+        self.password_file = pathlib.Path(f"{data_directory}/{password_filename}").absolute()
+        if not self.password_file.parent.exists():
+            self.password_file.parent.mkdir()
+        elif not self.key_file.exists():
+            ... # TODO: make a key generation method
+        else:
+            ... # TODO: make a load passwords method
