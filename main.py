3331a2bcb3c307e78b2ae344864ff56357b15171
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2022-12-24 20:41:36 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import settings as config
+import schif
+import ui
+import setup
+
+def main(settings: config.Settings) -> None:
+    logger = setup.logger(settings.log_directory, verbose=True)
+    manager = schif.Parol(
+        data_directory=settings.data_directory,
+        key_filename=settings.key_filename,
+        password_filename=settings.password_filename
+    )
+    ui.run_cli(manager, logger)
+
+if __name__ == "__main__":
+    setitngs = config.read(filepath="./settings", filename="settings", filetype="yaml", reader=config.YAMLReader)
