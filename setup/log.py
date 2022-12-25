4d2cc040a6588fea6216b9a9baca0d264be7bc84
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2022-12-24 20:32:35 -0500

    Create log.py

---
+++ setup/log.py
@@ -0,0 +1,25 @@
+import utils
+
+from datetime import datetime
+
+import pathlib
+
+def _make_log_directory(log_folder: str) -> None:
+    path = pathlib.Path(log_folder).absolute()
+    if not path.exists():
+        path.mkdir()
+
+def _make_logger(log_folder: str, date: str, verbose: bool) -> utils.Logger:
+    logger = utils.Logger(__name__)
+    log_file = f"{log_folder}/{date}.log"
+    if not verbose:
+        logger.file_only(log_file)
+    if verbose:
+        logger.full_setup(log_file)
+    return logger
+
+def logger(log_folder: str, verbose: bool = False) -> utils.Logger:
+    now = str(datetime.now().date())
+    _make_log_directory(log_folder)
+    logger = _make_logger(log_folder, now, verbose)
+    return logger
