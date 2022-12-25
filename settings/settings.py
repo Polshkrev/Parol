8d248c9c670046955625ab6bfd098e4c411df75c
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2022-12-24 19:50:27 -0500

    added settings package

---
+++ settings/settings.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+import typing
+
+import json
+import yaml
+
+Reader = typing.Callable[[typing.TextIO], dict[str, typing.Any]]
+JSONReader = json.load
+YAMLReader = yaml.safe_load
+
+@dataclass
+class Settings:
+    key_filename: str
+    password_filename: str
+    data_directory: str
+
+def read(filepath: str, filename: str, filetype: str = "json", reader: Reader = JSONReader) -> Settings:
+    file = f"{filepath}/{filename}.{filetype}"
+    with open(file, "r", encoding="utf-8") as f:
+        data = reader(f)
+        return Settings(**data)
