34db3d8541992cf87dbe24ae9f1cdf76f71b368a
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2022-12-28 21:11:09 -0500

    added an internal settings object

--- settings/settings.py
+++ settings/settings.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 import typing
 
@@ -10,14 +10,29 @@ JSONReader = json.load
 YAMLReader = yaml.safe_load
 
 @dataclass
+class Configuration:
+    appearance: str
+    theme: str
+    geometry: str
+    black_image: str
+    white_image: str
+
+@dataclass
 class Settings:
     key_filename: str
     password_filename: str
     log_directory: str
     data_directory: str
     verbose: bool
-    black_image: str
-    white_image: str
+    private_file: str = field(default_factory=str, repr=False)
+
+    def __post_init__(self) -> None:
+        self.configuration = self._get_config(reader=JSONReader)
+
+    def _get_config(self, reader: Reader = JSONReader) -> Configuration:
+        with open(self.private_file, "r", encoding="utf-8") as f:
+            data = reader(f)
+            return Configuration(**data)
 
 def read(filepath: str, filename: str, filetype: str = "json", reader: Reader = JSONReader) -> Settings:
     file = f"{filepath}/{filename}.{filetype}"
