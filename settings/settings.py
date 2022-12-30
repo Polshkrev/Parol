248ac644cb424a6d330e46efa2abe9ce370a7b8d
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2022-12-30 13:23:36 -0500

    added folder constant

--- settings/settings.py
+++ settings/settings.py
@@ -8,6 +8,7 @@ import yaml
 Reader = typing.Callable[[typing.TextIO], dict[str, typing.Any]]
 JSONReader = json.load
 YAMLReader = yaml.safe_load
+Folder = "./settings/public"
 
 @dataclass
 class Configuration:
