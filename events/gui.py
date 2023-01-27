f880ffb77e954905cb9b12a5ad40baa61bcdc48b
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2023-01-27 10:07:15 -0500

    pkutils change
    The structure of the dependent package pkutils changed. pkutils.globals is deprecated; events are moved to a separate file.

--- events/gui.py
+++ events/gui.py
@@ -12,8 +12,8 @@ def handle_new_card(root: ctk.CTkToplevel, gui: ui.gui.GUI, site: str, password:
 def handle_x_button_click(card: ui.components.card.Card, database: schif.Parol) -> None:
     card.remove()
     database.remove_password(card.title)
-    pkutils.patterns.post("remove_password", card.title)
+    pkutils.patterns.event.post("remove_password", card.title)
 
 def setup_gui_event_handlers(database: schif.Parol) -> None:
-    pkutils.patterns.subscribe("new_card", lambda args: handle_new_card(*args))
-    pkutils.patterns.subscribe("remove_card", lambda card: handle_x_button_click(card, database))
+    pkutils.patterns.event.subscribe("new_card", lambda args: handle_new_card(*args))
+    pkutils.patterns.event.subscribe("remove_card", lambda card: handle_x_button_click(card, database))
