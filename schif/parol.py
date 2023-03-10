fc4ae78124e6e51c620489bc3491f7c6a02bbac9
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2023-03-10 13:03:54 -0500

    file control flow easement

--- schif/parol.py
+++ schif/parol.py
@@ -4,21 +4,43 @@ import pathlib
 
 from schif.context import SQLite, sqlite3
 
+import typing
+
+CreationFunction = typing.Callable[[], None]
+
+def _check_parent(root: pathlib.Path) -> bool:
+    """Check if the parent of a given file exists."""
+    return root.parent.exists()
+
+def _make_parent(root: pathlib.Path) -> None:
+    """Make the parent directory of a given path if it doesn't exist already."""
+    if _check_parent(root):
+        return
+    root.parent.mkdir()
+
+def _check_root(root: pathlib.Path) -> bool:
+    """Check if a given file exists."""
+    return root.exists()
+
+def _make_root(file: pathlib.Path, create_function: CreationFunction) -> None:
+    """Make a given file if it doesn't exist already and it's parent directory exists."""
+    if _check_root(root=file):
+        return
+    create_function()
+
+def _check_file(path: pathlib.Path, create_function: CreationFunction) -> None:
+    """Interface to simplify the control flow of file creation."""
+    _make_parent(root=path)
+    _make_root(file=path, create_function=create_function)
+
 class Parol:
 
     def __init__(self, data_directory: str = "./data", key_filename: str = "KEY.key", password_filename: str = "passwords.db") -> None:
         self.key_file = pathlib.Path(f"{data_directory}/{key_filename}").absolute()
         self.password_file = pathlib.Path(f"{data_directory}/{password_filename}").absolute()
-        if not self.password_file.parent.exists():
-            self.password_file.parent.mkdir()
-            if not self.password_file.exists():
-                self._create_table()
-        elif not self.password_file.exists():
-            self._create_table()
-        elif not self.key_file.exists():
-            self.create_key()
-        else:
-            self.load_passwords(self.load_key())
+        _check_file(self.password_file, self._create_table)
+        _check_file(self.key_file, self.create_key)
+        self.load_passwords(self.load_key())
 
     def create_key(self) -> None:
         """Create an encryption key file."""
@@ -32,11 +54,15 @@ class Parol:
 
     def update_key_file(self, file: str) -> None:
         """Update the key file."""
-        self.key_file = pathlib.Path(file).absolute()
+        path = pathlib.Path(file).absolute()
+        _check_file(path, self.create_key)
+        self.key_file = path
 
     def update_password_file(self, file: str) -> None:
         """Update the password file."""
-        self.password_file = pathlib.Path(file).absolute()
+        path = pathlib.Path(file).absolute()
+        _check_file(path, self._create_table)
+        self.key_file = path
 
     def dump_passwords_to_file(self, key: bytes, data: dict[str, str]) -> None:
         """Dump passwords to a file."""
