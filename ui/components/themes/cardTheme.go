8acdfd94afc54edac4296a56bffc54593d286839
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-07-18 21:09:30 -0400

    Create cardTheme.go

---
+++ ui/components/themes/cardTheme.go
@@ -0,0 +1,41 @@
+package themes
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
+)
+
+// Custom card theme.
+type Card struct{}
+
+// Construct a new card theme.
+func NewCard() *Card {
+	return new(Card)
+}
+
+// Change the colour of the theme.
+// Returns a colour based on the given name and variant.
+func (Card) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
+	if variant == theme.VariantDark && name == theme.ColorNameShadow {
+		return color.White
+	}
+	return theme.DefaultTheme().Color(name, variant)
+}
+
+// Font method override.
+func (Card) Font(style fyne.TextStyle) fyne.Resource {
+	return theme.DefaultTheme().Font(style)
+}
+
+// Icon method override.
+func (Card) Icon(name fyne.ThemeIconName) fyne.Resource {
+	return theme.DefaultTheme().Icon(name)
+}
+
+// Size method override.
+func (Card) Size(name fyne.ThemeSizeName) float32 {
+	return theme.DefaultTheme().Size(name)
+
+}
