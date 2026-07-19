7e77b93933d03e6411e7a451d18cbd6500cfdde2
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-07-18 22:38:17 -0400

    Create gui.go

---
+++ setup/gui.go
@@ -0,0 +1,43 @@
+package setup
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"github.com/Polshkrev/gopolutils"
+	"github.com/Polshkrev/gopolutils/fayl"
+	"github.com/Polshkrev/gopolutils/table"
+	"github.com/Polshkrev/parol/models/password"
+	"github.com/Polshkrev/parol/schif/parol"
+	"github.com/Polshkrev/parol/settings"
+	"github.com/Polshkrev/parol/ui"
+)
+
+// Load the icon based on the given appearance.
+// Returns a [fyne.Resource] of the icon based on the given appearance.
+func loadIcon(appearance settings.Appearance, whitePath, darkPath *fayl.Path) fyne.Resource {
+	var resource fyne.Resource
+	var resourceError error
+	if appearance == settings.Dark {
+		resource, resourceError = fyne.LoadResourceFromPath(darkPath.String())
+		if resourceError != nil {
+			panic(gopolutils.NewNamedException(gopolutils.IOError, "%s", resourceError.Error()))
+		}
+	} else {
+		resource, resourceError = fyne.LoadResourceFromPath(whitePath.String())
+		if resourceError != nil {
+			panic(gopolutils.NewNamedException(gopolutils.IOError, "%s", resourceError.Error()))
+		}
+	}
+	return resource
+}
+
+// Setup the gui based on its parts.
+// Returns the gui of the application.
+func GUI(passwords table.Table[password.Password], manager *parol.Parol, configuration settings.Settings) *ui.GUI {
+	var application fyne.App = app.New()
+	var window fyne.Window = application.NewWindow(configuration.Configuration.Title)
+	window.SetIcon(loadIcon(configuration.Appearance, GetAssetPath(configuration.Configuration).JoinAs(configuration.Configuration.WhiteLogo), GetAssetPath(configuration.Configuration).JoinAs(configuration.Configuration.BlackLogo)))
+	var gui *ui.GUI = ui.NewGUI(&window, passwords, manager, configuration)
+	gui.Paint()
+	return gui
+}
