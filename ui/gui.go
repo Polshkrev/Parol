f53b14782b02fbe22c4f560e34f20bb92d57fb82
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-07-18 21:24:01 -0400

    Create gui.go

---
+++ ui/gui.go
@@ -0,0 +1,151 @@
+package ui
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+	"github.com/Polshkrev/gopolutils"
+	"github.com/Polshkrev/gopolutils/collections"
+	"github.com/Polshkrev/gopolutils/table"
+	"github.com/Polshkrev/parol/events"
+	"github.com/Polshkrev/parol/models/password"
+	"github.com/Polshkrev/parol/schif/parol"
+	"github.com/Polshkrev/parol/settings"
+	"github.com/Polshkrev/parol/ui/components"
+)
+
+// Custom GUI.
+type GUI struct {
+	settings  settings.Settings
+	parol     *parol.Parol
+	window    *fyne.Window
+	passwords table.Table[password.Password]
+	cards     collections.Collection[components.Component]
+}
+
+// Construct a gui based on its parts.
+// Returns the application gui.
+func NewGUI(window *fyne.Window, passwords table.Table[password.Password], parol *parol.Parol, settings settings.Settings) *GUI {
+	var gui *GUI = new(GUI)
+	gui.window = window
+	gui.parol = parol
+	gui.settings = settings
+	gui.passwords = passwords
+	gui.cards = collections.NewArray[components.Component]()
+	return gui
+}
+
+// Paint the gui.
+func (gui *GUI) Paint() {
+	(*gui.window).SetContent(paint(gui))
+	(*gui.window).Resize(fyne.NewSize(float32(gui.settings.Configuration.Width), float32(gui.settings.Configuration.Height)))
+	(*gui.window).CenterOnScreen()
+	events.Subscribe(events.ApplicationEnd, func() {
+		(*gui.window).Close()
+	})
+	(*gui.window).SetCloseIntercept(func() {
+		events.Post(events.ApplicationEnd)
+	})
+}
+
+// Show and run the gui.
+func (gui *GUI) Run() {
+	(*gui.window).ShowAndRun()
+}
+
+// Setup the gui based on its given parent container.
+func (gui *GUI) setup(parent *fyne.Container) {
+	var key string
+	for key = range gui.parol.Passwords() {
+		var card *components.Card = components.NewCard(key, string(gopolutils.Must(gui.parol.Get(key))), gui.settings.Appearance)
+
+		card.Paint(parent)
+		gui.cards.Append(card)
+		registerCardEvents(gui, card)
+	}
+}
+
+// Setup the base ui of the given gui.
+// Returns a [fyne.Container] based on each of its components.
+func paint(gui *GUI) *fyne.Container {
+	var gridSize fyne.Size = fyne.NewSize(float32((gui.settings.Configuration.Width/5)-5), float32((gui.settings.Configuration.Height/5)+100))
+	var content *fyne.Container = container.New(layout.NewGridWrapLayout(gridSize))
+	gui.setup(content)
+	var button *components.Button = components.NewButton("", theme.ContentAddIcon(), func() {
+		paintAddForm(gui, content, "Add Password", gui.settings.Appearance)
+	})
+	var label *widget.Label = widget.NewLabelWithStyle("Passwords:", fyne.TextAlignTrailing, fyne.TextStyle{Bold: true})
+	var header *components.Header = components.NewHeader(label, button, gui.settings.Configuration.Header.Width, gui.settings.Configuration.Header.Height)
+	header.Paint(nil)
+	return container.NewBorder(header.Parent(), nil, nil, nil, container.NewVScroll(content))
+}
+
+// Paint the add form based on its parent title, and appearance.
+func paintAddForm(gui *GUI, cardParent *fyne.Container, title string, appearance settings.Appearance) {
+	var formWindow fyne.Window = fyne.CurrentApp().NewWindow(title)
+	var form *components.Form
+	form = components.NewForm("Add Password", "Add", "Cancel", 300, 300, nil, onFormCancel(formWindow))
+	form.SetParent(&formWindow)
+	form.Append(components.NewItem(components.NewEntry("Key", false, false, func(s string) error { return nil })))
+	form.Append(components.NewItem(components.NewEntry("Password", true, true, func(s string) error { return nil })))
+	form.SetSubmitCallBack(registerAddFormSubmit(gui, cardParent, form, appearance))
+	form.Paint(nil)
+}
+
+// Paint the information dialog based on a title and message.
+func paintInformation(title, message string, width, height uint16) {
+	var parent fyne.Window = fyne.CurrentApp().NewWindow(title)
+	var information dialog.Dialog = dialog.NewInformation(title, message, parent)
+	information.SetOnClosed(func() {
+		parent.Close()
+	})
+	var infoSize fyne.Size = fyne.NewSize(float32(width), float32(height))
+	information.Resize(infoSize)
+	parent.Resize(infoSize)
+	information.Show()
+	parent.CenterOnScreen()
+	parent.RequestFocus()
+	parent.SetFixedSize(true)
+	parent.Show()
+}
+
+// Default callback for the form based on its given parent.
+// Returns the default callback triggered when the form is canceled.
+func onFormCancel(parent fyne.Window) components.Callback {
+	return func() {
+		parent.Close()
+	}
+}
+
+// Obtain the form items.
+// Returns a [collections.Pair] of each of the form items.
+func getFormItems(form *components.Form) *collections.Pair[*components.Item, *components.Item] {
+	return collections.NewPair(form.Items().Collect()[0], form.Items().Collect()[1])
+}
+
+// Setup the logic for submitting a form.
+// Returns a callback triggered when the form is submitted.
+func registerAddFormSubmit(gui *GUI, parent *fyne.Container, form *components.Form, appearance settings.Appearance) components.Callback {
+	return func() {
+		var items *collections.Pair[*components.Item, *components.Item] = getFormItems(form)
+		var card *components.Card = components.NewCard((*items.First()).Value(), (*items.Second()).Value(), appearance)
+		if len(card.Key()) == 0 {
+			paintInformation("Empty Key", "The Key Entry Can Not Be Empty.", 400, 150)
+			return
+		} else if len(card.Password()) == 0 {
+			paintInformation("Empty Password", "The Password Entry Can Not Be Empty.", 400, 150)
+			return
+		}
+		registerCardEvents(gui, card)
+		card.Paint(parent)
+		var except *gopolutils.Exception = gui.parol.Insert(card.Key(), card.Password())
+		if except != nil {
+			panic(except)
+		}
+		gui.cards.Append(card)
+		(*form.Parent()).Close()
+	}
+}
