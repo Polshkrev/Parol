8dbde7756327a676d8f6c32bc4a095f289c5deff
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-07-20 02:10:39 -0400

    Update defaults.go

--- setup/defaults.go
+++ setup/defaults.go
@@ -2,6 +2,19 @@ package setup
 
 import "github.com/Polshkrev/goserialize"
 
+const (
+	applicationName string = "Parol"            // Name of the current application.
+	authorFolder    string = "polshkrev"        // Author of the current application.
+	assetsFolder    string = "assets"           // Folder where the assets are stored.
+	copyBlackIcon   string = "black.png"        // Black icon file name.
+	copyWhiteIcon   string = "white.png"        // White icon file name.
+	blackLogo       string = "p_logo_black.png" // Black logo file.
+	whiteLogo       string = "p_logo_white.png" // White Logo file.
+	dataFolder      string = "data"             // Data folder.
+	databaseFile    string = "database.db"      // Database filename.
+	keyFile         string = "Key.key"          // Key filename.
+)
+
 // Default settings for the application.
 var defaultSettings goserialize.Object = goserialize.Object{
 	"appearance": "light",
@@ -11,14 +24,6 @@ var defaultSettings goserialize.Object = goserialize.Object{
 			"width":  50,
 			"height": 50,
 		},
-		"assetFolder":   "assets",
-		"blackIcon":     "black.png",
-		"whiteIcon":     "white.png",
-		"blackLogo":     "p_logo_black.png",
-		"whiteLogo":     "p_logo_white.png",
-		"dataFolder":    "data",
-		"databaseFile":  "database.db",
-		"keyFile":       "Key.key",
 		"appearanceKey": "FYNE_THEME",
 		"width":         1500,
 		"height":        750,
