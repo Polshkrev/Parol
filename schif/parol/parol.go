048f1af7d5279e9db047d605c7d109c1401ddcd8
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-07-20 03:00:49 -0400

    Update parol.go

--- schif/parol/parol.go
+++ schif/parol/parol.go
@@ -1,6 +1,9 @@
 package parol
 
 import (
+	"cmp"
+	"slices"
+
 	"github.com/Polshkrev/gopolutils"
 	"github.com/Polshkrev/gopolutils/collections"
 	"github.com/Polshkrev/goserialize"
@@ -27,15 +30,17 @@ func New(key *fernet.Key) *Parol {
 // If the key is already in the manager, instead of just quietly not inserting into the manager, a [gopolutils.KeyError] is returned.
 // If a [gopolutils.KeyError] is returned, the manager is not modified.
 func (parol *Parol) Insert(key, password string) *gopolutils.Exception {
-	return parol.passwords.Insert(key, gopolutils.Must(encryptPassword(parol.key, password)))
+	var token []byte = gopolutils.Must(encryptPassword(parol.key, password))
+	return parol.passwords.Insert(key, token)
 }
 
 // Append a [collections.View] of [password.Password]s to the manager.
 // If the password can not be appended to the manager, a [gopolutils.KeyError] is returned.
-func (parol *Parol) Extend(passwords collections.View[password.Password]) *gopolutils.Exception {
-	var i gopolutils.Size
-	for i = range collections.Enumerate(passwords) {
-		var password password.Password = passwords.Collect()[i]
+func (parol *Parol) Extend(passwords collections.View[*password.Password]) *gopolutils.Exception {
+	var raw []*password.Password = passwords.Collect()
+	var i int
+	for i = range raw {
+		var password *password.Password = raw[i]
 		var except *gopolutils.Exception = parol.passwords.Insert(password.Key(), []byte(password.Password()))
 		if except != nil {
 			return except
@@ -56,16 +61,15 @@ func (parol Parol) Get(key string) ([]byte, *gopolutils.Exception) {
 	if except != nil {
 		return nil, except
 	}
-	return decryptPassword(parol.key, *token)
+	return decryptPassword(*token, parol.key)
 }
 
 // Collect the passwords stored in the manager.
 // Returns a [goserialize.Object] of each of the key-value pair stored in the manager.
 func (parol Parol) Passwords() goserialize.Object {
+	var bucket collections.Pair[string, []byte]
 	var result goserialize.Object = make(goserialize.Object, 0)
-	var i gopolutils.Size
-	for i = range collections.Enumerate(parol.passwords) {
-		var bucket collections.Pair[string, []byte] = parol.passwords.Collect()[i]
+	for _, bucket = range parol.passwords.Collect() {
 		var key *string
 		var value *[]byte
 		key, value = bucket.Items()
@@ -98,10 +102,37 @@ func encryptPassword(key *fernet.Key, password string) ([]byte, *gopolutils.Exce
 // Decrypt a given password using a given key.
 // Returns a slice of bytes containing the decrypted password.
 // If the given password can not be decrypted, a [gopolutils.KeyError] is returned with a nil data pointer.
-func decryptPassword(key *fernet.Key, password []byte) ([]byte, *gopolutils.Exception) {
+func decryptPassword(password []byte, key *fernet.Key) ([]byte, *gopolutils.Exception) {
 	var result []byte = fernet.VerifyAndDecrypt(password, 0, []*fernet.Key{key})
 	if result == nil {
 		return nil, gopolutils.NewNamedException(gopolutils.KeyError, "Can not decrypt password \"%s\" at key \"%s\"", string(password), key)
 	}
 	return result, nil
+}
+
+// Sort a given [collections.View] of [password.Password]s.
+// Returns a sorted copy of the given [collections.View] of [password.Password]s.
+func sort(values collections.Collection[password.Password]) collections.View[password.Password] {
+	var raw []password.Password = values.Collect()
+	var result collections.Collection[password.Password] = collections.NewArray[password.Password]()
+	slices.SortFunc(raw, func(a, b password.Password) int {
+		return cmp.Compare(a.Key(), b.Key())
+	})
+	var i int
+	for i = range raw {
+		result.Append(raw[i])
+	}
+	return result
+}
+
+// Convert a given [goserialize.Object] into a [collections.View] of [password.Password]s.
+// Returns [collections.View] of [password.Password]s containing each of the values of the given [goserialize.Object].
+func ObjectToView(object goserialize.Object) collections.View[password.Password] {
+	var result collections.Collection[password.Password] = collections.NewArray[password.Password]()
+	var key string
+	var value any
+	for key, value = range object {
+		result.Append(*password.New(key, value.(string)))
+	}
+	return sort(result)
 }
